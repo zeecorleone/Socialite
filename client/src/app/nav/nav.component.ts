072768ff771acfd512f7cb1b91e97207a322a765
Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
2024-06-22 12:37:21 +0500

    cleaning up loggedIn bit by replacing it with accountservice.CurrentUser directly

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +11,11 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  loggedIn: boolean = false;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    
   }
   
 
@@ -22,7 +23,7 @@ export class NavComponent implements OnInit {
     
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
+      
     }, error => {
 
     });
@@ -30,16 +31,9 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
-    this.loggedIn = false;
+
     this.accountService.logout();
   }
 
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    });
-  }
 
 }
